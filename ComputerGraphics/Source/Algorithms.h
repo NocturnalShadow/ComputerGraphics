68a658a099196b8b613970a36c4c676afad61b4f
NocturnalShadow <bogdanscloud@gmail.com>
2018-02-09 01:29:13 +0200

    Version 1.0

---
+++ ComputerGraphics/Source/Algorithms.h
@@ -0,0 +1,11 @@
+#include <vector>
+#include <Eigen/Dense>
+
+namespace Geometry
+{
+	inline float angleOf(Eigen::Vector2f vector) {
+		return atan2(vector.y(), vector.x());
+	}
+
+	bool isInside(const std::vector<Eigen::Vector2f>& polygon, Eigen::Vector2f point);
+}
